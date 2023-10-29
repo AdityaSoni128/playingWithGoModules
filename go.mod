df2582bf5d2c0129d12140c8fd2551714ae597c3
Aditya Soni <showoff128@gmail.com>
2023-10-29 14:13:56 +0530

    added tablegetter function

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module github.com/AdityaSoni128/tablebuilderwithgo
+module github.com/AdityaSoni128/playingWithGoModules
 
 go 1.21.1
+
+require github.com/AdityaSoni128/calculatorwithgo v1.1.2
+
+require github.com/AdityaSoni128/tablebuilderwithgo v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	calculator "github.com/AdityaSoni128/calculatorwithgo"
+)
 
 func main() {
 	fmt.Println("Hello I am Playing with Go Modules")
+	val := calculator.Add(10, 20)
+	fmt.Println(val)
+	calculator.Tablegetter(4)
+
 }
